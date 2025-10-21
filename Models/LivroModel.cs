c748ead8f87e42ebdebf546708bc26d94c608845
cassio <bindaco77@gmail.com>
2025-10-21 17:33:52 -0300

    Primeiro commit

---
+++ Models/LivroModel.cs
@@ -0,0 +1,50 @@
+using System.Text.Json;
+using Newtonsoft.Json;
+
+namespace Estudo_Serializacao_Desserializacao.Models
+{
+    public class LivroModel
+    {
+        [JsonProperty(nameof(IdLivro))]
+        public int IdLivro { get; set; }
+
+        [JsonProperty(nameof(Titulo))]
+        public string Titulo { get; set; }
+
+        [JsonProperty(nameof(Autor))]
+        public string Autor { get; set; }
+
+        [JsonProperty(nameof(DataPublicacao))]
+        public DateOnly DataPublicacao { get; set; }
+
+        [JsonIgnore]
+        public string SenhaInterna { get; set; } = "1111";
+
+        [JsonConstructor]
+        public LivroModel(int IdLivro, string Titulo, string autor)
+        {
+            this.IdLivro = IdLivro;
+            this.Autor = autor;
+            this.Titulo = Titulo;
+        }
+        
+        private static JsonSerializerSettings settings { get; } = new JsonSerializerSettings
+        {
+            DateFormatString = "dd/mm/yyyy",
+            Formatting = Formatting.Indented,
+            NullValueHandling = NullValueHandling.Ignore
+        };
+
+        public static string Serealizar(LivroModel livro)
+        {
+            return JsonConvert.SerializeObject(livro, settings);
+        }
+
+        public static LivroModel Desserializar(string livro)
+        {
+            return JsonConvert.DeserializeObject<LivroModel>(livro, settings);
+        }
+        
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using Estudo_Serializacao_Desserializacao.Models;
+
+var Livro = new LivroModel(1, "Pequeno principe", "Json");
+
+
+Console.WriteLine("--- 1. SERIALIZAÇÃO BÁSICA (Objeto C# -> JSON) ---");
+
+string SerealizacaoObj = LivroModel.Serealizar(Livro);
+
+Console.WriteLine(SerealizacaoObj);
+
+Console.WriteLine();
+
+Console.WriteLine("\n--- 2. DESSERIALIZAÇÃO (JSON -> Objeto C#) ---");
+
+string jsonRecebido = @"{
+    ""IdLivro"": ""202"",
+    ""Autor"": ""José"",
+    ""Titulo"": ""Padrões de Projeto"",
+    ""DataPublicacao"": ""21-10-1994""
+}";
+
+try
+{
+    var livroDesserializado = LivroModel.Desserializar(jsonRecebido);
+
+    Console.WriteLine($"Autor do livro: {livroDesserializado?.Autor}");
+    Console.WriteLine($"Título do livro: {livroDesserializado?.Titulo}");
+    Console.WriteLine($"ID: {livroDesserializado?.IdLivro} (Desserializado com sucesso de string para int!)");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Erro durante a desserialização: {ex.Message}");
+}
